638c6eafe13f315d4f068efc5cd73c723962eb73
Umoren <samuelumoren365@gmail.com>
2022-11-21 09:13:00 +0100

    domains working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["nextimagedemo1112419-dev.s3.amazonaws.com"]
+  }
 }
 
 module.exports = nextConfig
